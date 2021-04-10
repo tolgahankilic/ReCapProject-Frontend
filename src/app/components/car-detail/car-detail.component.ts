1a187b74f539b9dfe98ec46edb5e0cc8e87926c3
Tolgahan Kılıç <kilictolgahan@outlook.comgit configgit config --global user.email kilictolgahan@outlook.com>
2021-04-10 03:19:46 +0300

    Car-detail refactor

--- src/app/components/car-detail/car-detail.component.ts
+++ src/app/components/car-detail/car-detail.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Car } from 'src/app/models/car';
 import { CarDetail } from 'src/app/models/carDetail';
+import { CarImage } from 'src/app/models/carImage';
 import { CarDetailService } from 'src/app/services/car-detail.service';
+import { CarImageService } from 'src/app/services/car-image.service';
 
 
 @Component({
@@ -12,20 +14,21 @@ import { CarDetailService } from 'src/app/services/car-detail.service';
 })
 export class CarDetailComponent implements OnInit {
 
-  carDetails:CarDetail [];
-  carDetails1:CarDetail
+  carDetails:CarDetail
+  carImages:CarImage[];
   currentCar:Car
-  car:Car[]
   dataLoaded=false
  
   imageUrl = "https://localhost:44382";
   constructor(private carDetailService:CarDetailService,
+    private carImageService:CarImageService,
     private activatedRoute:ActivatedRoute) { }
 
     ngOnInit(): void {
       this.activatedRoute.params.subscribe(params => {
         if(params["carId"]){
           this.getCarDetails(params["carId"]);
+          this.getImagesById(params["carId"]);
         }
       })
     }
@@ -33,9 +36,16 @@ export class CarDetailComponent implements OnInit {
     getCarDetails(carId:number)
     {
       this.carDetailService.getCarDetails(carId).subscribe(response => {
-        this.carDetails = response.data;
+        this.carDetails = response.data[0];
         this.dataLoaded=true;
         console.log(response);
+      })
+    }
+
+    getImagesById(id:number){
+      this.carImageService.getCarImagesByCarId(id).subscribe(response=>{
+        this.carImages=response.data;
+        
       })
     }
 
